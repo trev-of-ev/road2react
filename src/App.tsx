198be9e79ed5ce3e1b4a22befab72e7b86f66464
Trevor Guinn <tdguinn87@gmail.com>
2024-11-02 23:35:10 -0500

    ADD MAP METHOD TO LIST ARRAY

--- src/App.tsx
+++ src/App.tsx
@@ -22,10 +22,27 @@ const list = [
 const App = () => {
   return (
     <div>
-      <h1>Hello {getTitle("React")}</h1>
+      <h1>My Hacker Stories</h1>
 
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
+
+      <hr />
+
+      <ul>
+        {list.map(function (item) {
+          return (
+            <li key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
